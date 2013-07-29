84415e89abdfe522afe583a012cecad55feffdc3
Dwylkz <sgeteternal@126.com>
2013-07-29 22:12:41 +0800

    hdu 4616

--- Hdu/2013 Multi-University Training Contest/2/1006/main.cc
+++ Hdu/2013 Multi-University Training Contest/2/1006/main.cc
@@ -32,7 +32,7 @@ typedef char char_a[maxn];
 typedef pair<int, int> II;
 
 //macro
-#define WHERE	puts(__func__);
+#define WHERE    puts(__func__);
 #define PA(H, I, S, E, F,...) {\
   puts(H);\
   for (int I = (S); I < (E); I++) {\
@@ -47,8 +47,7 @@ struct edge {
   int v, to;
 };
 struct node {
-  LL val;
-  int trp;
+  int val, trp;
 } V[maxn];
 vector<edge> E;
 int_a L; 
@@ -67,20 +66,51 @@ void graph_add_edge(int u, int v)
 int n, c;
 
 LL ans, dp[maxn][4][2];
+/* dp[node_number][trapped_times][end_without_trap or end_with_trap] */
 void dp_init()
 {
-  ans = -1;
-  memeset(dp, 0, sizeof(dp));
+  ans = 0;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < 4; j++)
+      for (int k = 0; k < 2; k++)
+        dp[i][j][k] = -inf64;
 }
 void dp_run(int u = 0, int p = -1)
 {
-  for (int i = L[u]; i != -1; i++) {
+  LL (&f)[4][2] = dp[u],
+     val = V[u].val,
+     trp = V[u].trp;
+  f[trp][trp] = val;
+  ans = max(ans, f[trp][trp]);
+  for (int i = L[u]; i != -1; i = E[i].to) {
+    if (i == p) continue;
+    dp_run(E[i].v, i ^ 1);
+    LL (&g)[4][2] = dp[E[i].v];
+    for (int j = 0; j <= c; j++)
+      for (int k = 0; k+j <= c; k++) {
+        if (k+j <= c) ans = max(ans, f[j][1]+g[k][1]);
+        if (k+j < c) ans = max(ans, f[j][0]+g[k][0]);
+        if (j != c) ans = max(ans, f[j][0]+g[k][1]);
+        if (k != c) ans = max(ans, f[j][1]+g[k][0]);
+      }
+    for (int j = 0; j < c; j++) {
+      f[j+trp][0] = max(f[j+trp][0], g[j][0]+val);
+      f[j+trp][1] = max(f[j+trp][1], g[j][1]+val);
+    }
+    if (!trp) f[c][1] = max(f[c][1], g[c][1]+val);
   }
+#if 0
+  printf("node %4d{%4lld, %4lld}\n", u, val, trp);
+  for (int i = 0; i <= c; i++) {
+    printf("%4d end_without_trap = %4lld, end_with_trap = %4lld\n",
+        i, f[i][0], f[i][1]);
+  }
+#endif
 }
 
 //main
 int main() {
-#if 1
+#if 0
   freopen(input, "r", stdin);
   //freopen(output, "w", stdout);
 #endif
@@ -90,7 +120,7 @@ int main() {
     scanf("%d%d", &n, &c);
     graph_init();
     for (int i = 0; i < n; i++) {
-      scanf("%lld%d", &V[i].val, &V[i].trp);
+      scanf("%d%d", &V[i].val, &V[i].trp);
     }
     for (int i = 0; i < n - 1; i++) {
       int u, v;
