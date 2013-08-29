725f6dba4fde0a8d86949c5a348a6d1527dda3fa
Dwylkz <sgeteternal@126.com>
2013-08-29 08:09:06 +0800

    Thu Aug 29 08:09:06 CST 2013

--- 0_Custom/Combination/spoj kpsum/main.cc
+++ 0_Custom/Combination/spoj kpsum/main.cc
@@ -28,37 +28,35 @@ LL askp(LL p, int f) {
   for (LL t = p, i = 0; o; t %= o, o /= 10, i ^= 1)
     if (i&1) rv -= t/o;
     else rv += t/o;
+  if (!f) return rv;
   for (int i = 0; i < f; i++) rv *= 10;
-  if (f+d&1) {
-    if (f) rv = askf(d+f, f);
-    else rv += askf(d+f, f);
-  } else rv += askf(d+f, f);
+  if ((d+f&1) && (f)) rv = 0;
+  rv += askf(d+f, f);
   return rv;
 }
 LL askn(LL n) {
-  if (n < 10) return n;
   LL rv = 0, d = 1, o = 1;
+  if (n < 10) {
+    for (int i = 1, b = 1; i <= n; i++, b ^= 1)
+      rv += b? i: -i;
+    return rv;
+  }
   for (LL t = n; t > 9; t /= 10) d++, o *= 10;
   for (int i = 1; i < d; i++)
-    if (i == 1) rv -= 5;
+    if (i == 1) rv += 5;
     else for (int j = 1; j < 10; j++)
-      rv += askp(j, i-1);
-  for (LL t = n, p = 0; o; p = p*10+t/o, t %= o, o /= 10, d--)
-    for (int i = p == 0? 1: 0; i < t/o; i++)
-      rv += askp(p*10+i, d-1);
-  return rv;
-}
-LL check(int n) {
-  LL rv = 0, r = 0;
-  for (int i = 0; i <= n; i++) {
-    LL o = 1;
-    for (int t = i; t > 9; t /= 10) o *= 10;
-    for (int t = i; o; t %= o, o /= 10) {
-      if (r) rv -= t/o;
-      else rv += t/o;
-      r ^= 1;
-    }
-  }
+      rv -= askp(j, i-1);
+  int oe = d&1;
+  for (LL t = ++n, p = 0; o; p = p*10+t/o, t %= o, o /= 10, d--)
+    for (int i = p == 0? 1: 0, b = 1; i < t/o; i++)
+      if (oe) {
+        if (d-1) rv -= askp(p*10+i, d-1);
+        else {
+          if (b) rv -= askp(p*10+i, d-1);
+          else rv += askp(p*10+i, d-1);
+          b ^= 1;
+        }
+      } else rv -= askp(p*10+i, d-1);
   return rv;
 }
 
@@ -66,10 +64,8 @@ int main() {
 #if 1
   freopen("input.in", "r", stdin);
 #endif
-//   int k = 202020;
-//   printf("%lld == %lld?\n", askn(k+1), check(k));
   for ( ; ~scanf("%lld", &n), n; ) {
-    printf("%lld\n", askn(n+1));
+    printf("%lld\n", askn(n));
   }
   return 0;
 }
