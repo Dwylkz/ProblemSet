6d8ce624c8615b203be0871019aa61cbe38db257
Dwylkz <xellosgeteternal@gmail.com>
2014-03-03 23:28:33 +0800

    Mon Mar  3 23:28:32 CST 2014

---
+++ Help/cn/main.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <dac/List.h>
+
+char s[] = {"aasdfasf", "askdfjalsdf"}, sl = 2;
+
+int main() {
+  List *l = ListNew(StringT);
+  for (int i = 0; i < 2; i++)
+  return 0;
+}
