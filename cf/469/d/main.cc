a57ed6051f38267aa1a025a02fdc807afcbe993d
Dwylkz <xellosgeteternal@gmail.com>
2014-10-07 23:05:19 +0800

    solved cf 469 d

--- cf/469/d/main.cc
+++ cf/469/d/main.cc
@@ -1,149 +1,24 @@
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+// p -> q => !q -> !p
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const int N = 2e5+10;
+const int N = 1e5+10;
 
-struct Node {
-  vector<Node*> to;
-  vector<Node*> rto;
+int n, a, b, p[N], f[N];
+map<int, int> m;
+set<int> h;
 
-  int color;
-  int in;
-  int out;
-  bool inpath;
-  Node* group;
-
-  int indegree;
-  int outdegree;
-
-  void Init()
-  {
-    to.clear();
-    rto.clear();
-
-    color = -1;
-    in = out = -1;
-    inpath = false;
-    group = this;
-
-    indegree = outdegree = 0;
-  }
-};
-
-Node nodes[N];
-int n, a, b, p[N];
-unordered_map<int, int> rp;
-
-void Add(int u, int v)
+int Find(int x)
 {
-  nodes[2*u].to.push_back(nodes+2*v);
-  nodes[2*u+1].to.push_back(nodes+2*v+1);
+  if (x == f[x])
+    return x;
+  return f[x] = Find(f[x]);
 }
 
-int Build()
+void Union(int x, int y)
 {
-  for (int i = 0; i < n; i++) {
-    auto pa = rp.find(a-p[i]);
-    auto pb = rp.find(b-p[i]);
-    if (pa == rp.end() && pb == rp.end())
-      return -1;
-    if (pa != rp.end())
-      Add(i, pa->second);
-    if (pb != rp.end())
-      Add(i, pb->second);
-  }
-  return 0;
-}
-
-int stamp = 0;
-vector<Node*> path;
-void Tarjan(Node* u)
-{
-  u->in = u->out = stamp++;
-  u->inpath = true;
-  path.push_back(u);
-  for (auto& v: u->to)
-    if (v->in == -1) {
-      Tarjan(v);
-      u->out = min(u->out, v->out);
-    }
-    else if (v->inpath)
-      u->out = min(u->out, v->in);
-
-  if (u->in != u->out)
-    return ;
-
-  Node* group = u;
-  do {
-    u = path.back();
-    path.pop_back();
-    u->inpath = false;
-    u->group = group;
-  } while (u != group);
-}
-
-int Check()
-{
-  for (int i = 0; i < n; i++)
-    if (nodes[2*i].group == nodes[2*i+1].group)
-      return -1;
-  return 0;
-}
-
-void Dfs(Node* u)
-{
-  u->color = 1;
-  nodes[(u-nodes)^1].color = 0;
-  for (auto& v: u->rto)
-    Dfs(v);
-}
-
-void Color()
-{
-  for (Node* u = nodes; u < nodes+2*n; u++)
-    if (u == u->group)
-      for (auto& v: u->to)
-        v->group->rto.push_back(u->group);
-
-  for (Node* u = nodes; u < nodes+2*n; u++)
-    if (u == u->group) {
-      sort(u->rto.begin(), u->rto.end());
-      u->rto.erase(unique(u->rto.begin(), u->rto.end()), u->rto.end());
-    }
-
-  vector<Node*> seq;
-  for (Node* u = nodes; u < nodes+2*n; u++)
-    if (u == u->group)
-      for (auto& v: u->rto) {
-        u->outdegree++;
-        v->indegree++;
-      }
-
-  for (Node* u = nodes; u < nodes+2*n; u++)
-    if (u == u->group && u->indegree == 0)
-      seq.push_back(u);
-
-  for (auto& u: seq)
-    for (auto& v: u->to) {
-      v->indegree--;
-      if (v->indegree == 0)
-        seq.push_back(v);
-    }
-
-  for (auto& u: seq)
-    if (u->color == -1)
-      Dfs(u);
+  f[Find(x)] = Find(y);
 }
 
 int main()
@@ -151,28 +26,35 @@ int main()
   scanf("%d%d%d", &n, &a, &b);
   for (int i = 0; i < n; i++) {
     scanf("%d", p+i);
-    rp[p[i]] = i;
-    nodes[2*i].Init();
-    nodes[2*i+1].Init();
+    m[p[i]] = i;
+    h.insert(p[i]);
   }
 
-  if (Build() != 0) {
-    printf("NO");
+  int fa = n, fb = n+1;
+  for (int i = 0; i < n+2; i++)
+    f[i] = i;
+
+  for (int i = 0; i < n; i++) {
+    int ra = a-p[i];
+    int rb = b-p[i];
+    if (h.count(ra))
+      Union(m[p[i]], m[ra]);
+    else
+      Union(m[p[i]], fb);
+
+    if (h.count(rb))
+      Union(m[p[i]], m[rb]);
+    else
+      Union(m[p[i]], fa);
+  }
+
+  if (Find(fa) == Find(fb)) {
+    puts("NO");
     return 0;
   }
 
-  for (int i = 0; i < 2*n; i++)
-    if (nodes[i].in == -1)
-      Tarjan(nodes+i);
-
-  if (Check() != 0) {
-    printf("NO");
-    return 0;
-  }
-
-  Color();
   puts("YES");
   for (int i = 0; i < n; i++)
-    printf("%d%c", nodes[2*i].color == 1? 1: 0, i < n-1? ' ': '\n');
+    printf("%d%c", Find(i) == Find(fa)? 0: 1, i < n-1? ' ': '\n');
   return 0;
 }
