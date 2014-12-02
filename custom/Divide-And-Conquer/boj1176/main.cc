02f9800fc1f9d7111a1887c65b5f79b9e824d0c4
Dwylkz <xellosgeteternal@gmail.com>
2014-12-02 16:32:02 +0800

    use inplace_merge instead of sort

--- custom/Divide-And-Conquer/boj1176/main.cc
+++ custom/Divide-And-Conquer/boj1176/main.cc
@@ -18,10 +18,6 @@ const int N = 18e4+5;
 struct Fenwick {
   static const int M = 2e6+5;
   int d[M];
-  void Init()
-  {
-    memset(d, 0, sizeof(d));
-  }
   void Add(int x, int y)
   {
     for (x++; x < M; x += -x&x)
@@ -36,77 +32,69 @@ struct Fenwick {
   }
 };
 
-struct Query {
-  int op, x, y, w, z, a;
-};
-
 struct Line {
-  int x, y1, y2, i, c;
+  int op, x, y1, y2, i, c;
   Line()
   {
   }
-  Line(int x_, int y1_, int y2_, int i_, int c_):
-      x(x_), y1(y1_), y2(y2_), i(i_), c(c_)
+  Line(int op_, int x_, int y1_, int y2_, int i_, int c_):
+      op(op_), x(x_), y1(y1_), y2(y2_), i(i_), c(c_)
   {
   }
   friend bool operator < (const Line& lhs, const Line& rhs)
   {
     if (lhs.x != rhs.x)
       return lhs.x < rhs.x;
-    return lhs.i < rhs.i;
+    return lhs.y1 < rhs.y1;
   }
 };
 
-Query q[N];
-int s, w;
-int ans[N];
+Line p[N];
+int s, w, n, m, ans[N];
 
 Fenwick f;
-Line p[N];
 void DAC(int l, int r)
 {
   if (r-l < 2)
     return ;
   int m = (l+r)/2;
-  int np = 0;
-  for (int i = l; i < m; i++)
-    if (q[i].op == 1)
-      p[np++] = Line(q[i].x, q[i].y, 0, i, 0);
-  for (int i = m; i < r; i++)
-    if (q[i].op == 2) {
-      p[np++] = Line(q[i].x-1, q[i].y-1, q[i].z, i, -1);
-      p[np++] = Line(q[i].w, q[i].y-1, q[i].z, i, 1);
-    }
-  sort(p, p+np);
-  for (int i = 0; i < np; i++)
-    if (q[p[i].i].op == 1)
-      f.Add(p[i].y1, q[p[i].i].a);
-    else
-      ans[q[p[i].i].a] += p[i].c*(f.Ask(p[i].y2)-f.Ask(p[i].y1));
-  for (int i = 0; i < np; i++)
-    if (q[p[i].i].op == 1)
-      f.Add(p[i].y1, -q[p[i].i].a);
   DAC(l, m);
   DAC(m, r);
+  int j = l;
+  for (int i = m; i < r; i++)
+    if (p[i].op == 2) {
+      while (j < m && p[j].x <= p[i].x) {
+        if (p[j].op == 1)
+          f.Add(p[j].y1, p[j].c);
+        j++;
+      }
+      ans[p[i].i] += p[i].c*(f.Ask(p[i].y2)-f.Ask(p[i].y1-1));
+    }
+  while (j > l) {
+    if (p[j-1].op == 1)
+      f.Add(p[j-1].y1, -p[j-1].c);
+    j--;
+  }
+  inplace_merge(p+l, p+m, p+r);
 }
 
 int main()
 {
   scanf("%d%d", &s, &w);
-  int n = 0, m = 0;
-  while (~scanf("%d", &q[n].op) && q[n].op != 3) {
-    if (q[n].op == 1)
-      scanf("%d%d%d", &q[n].x, &q[n].y, &q[n].a);
-    else {
-      scanf("%d%d%d%d", &q[n].x, &q[n].y, &q[n].w, &q[n].z);
-      q[n].a = m;
-      ans[m++] = 1ll*(q[n].z+1-q[n].y)*(q[n].w+1-q[n].x)*s;
+  int op, x, y, w, z, a;
+  while (~scanf("%d", &op) && op != 3)
+    if (op == 1) {
+      scanf("%d%d%d", &x, &y, &a);
+      p[n++] = Line(op, x, y, 0, 0, a);
     }
-    n++;
-  }
+    else {
+      scanf("%d%d%d%d", &x, &y, &w, &z);
+      p[n++] = Line(op, x-1, y, z, m, -1);
+      p[n++] = Line(op, w, y, z, m, 1);
+      ans[m++] = (z+1-y)*(w+1-x)*s;
+    }
   DAC(0, n);
   for (int i = 0; i < m; i++)
     printf("%d\n", ans[i]);
-
   return 0;
 }
