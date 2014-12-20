fe499180510d8c58cfac110cd17455a498231db0
Dwylkz <xellosgeteternal@gmail.com>
2014-12-20 16:12:28 +0800

    fix 497b

--- cf/497/b/main.cc
+++ cf/497/b/main.cc
@@ -14,47 +14,61 @@
 using namespace std;
 
 const int N = 1e5+5;
+const int M = 2;
 
-int n, a[N];
+int n, a[N], sa[M][N];
 
-bool Check(int s, int t, int win)
+int Search(int lb, int rb, int t, int i, int l)
 {
-  int p[2] = {0}, r[2] = {0};
-  for (int i = 0; i < n; i++) {
-    p[a[i]]++;
-    if (p[a[i]] == t) {
-      memset(p, 0, sizeof(p));
-      r[a[i]]++;
+  if (lb == rb)
+    return lb;
+  int m = (lb+rb)/2;
+  if (sa[i][m]-sa[i][l] >= t)
+    return Search(lb, m, t, i, l);
+  return Search(m+1, rb, t, i, l);
+}
+
+bool IsOk(int t, int win, int& s)
+{
+  int ts[M] = {0}, i = 0;
+  while (i < n) {
+    int ls = Search(i+1, n+1, t, 0, i);
+    int rs = Search(i+1, n+1, t, 1, i);
+    if (ls == n+1 && rs == n+1)
+      return false;
+    if (rs == n+1 || ls < rs) {
+      ts[0]++;
+      i = ls;
+    }
+    else {
+      ts[1]++;
+      i = rs;
     }
   }
-  return (r[1-win] < r[win] && r[win] == s);
+  s = ts[win];
+  return ts[win] > ts[1-win];
 }
 
 int main()
 {
   scanf("%d", &n);
-  int p[2] = {0};
   for (int i = 0; i < n; i++) {
     scanf("%d", a+i);
     a[i]--;
-    p[a[i]]++;
+    for (int j = 0; j < 2; j++)
+      sa[j][i+1] = sa[j][i];
+    ++sa[a[i]][i+1];
   }
   int win = a[n-1];
   vector<pair<int, int>> ans;
-  for (int i = 1; 1ll*i*i <= p[win]; i++)
-    if (p[win]%i == 0) {
-      int s = i, t = p[win]/i;
-      if (Check(s, t, win))
-        ans.push_back(make_pair(s, t));
-      if (s == t)
-        continue;
-      swap(s, t);
-      if (Check(s, t, win))
-        ans.push_back(make_pair(s, t));
-    }
+  for (int t = 1; t <= sa[win][n]; t++) {
+    int s;
+    if (IsOk(t, win, s))
+      ans.push_back(make_pair(s, t));
+  }
   sort(ans.begin(), ans.end());
   printf("%d\n", (int)ans.size());
-  for (int i = 0; i < (int)ans.size(); i++)
-    printf("%d %d\n", ans[i].first, ans[i].second);
+  for (auto& p: ans)
+    printf("%d %d\n", p.first, p.second);
   return 0;
 }
