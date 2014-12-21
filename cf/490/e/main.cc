38593a665678ec9b0aefb7d9ea5f397a0b3eb5f7
Dwylkz <xellosgeteternal@gmail.com>
2014-12-21 17:00:08 +0800

    fix cf490e

--- cf/490/e/main.cc
+++ cf/490/e/main.cc
@@ -20,28 +20,59 @@ const int M = 8+5;
 int n;
 char d[N][M];
 
-template<typename T, typename F> T Conv(F from)
+void MinRepair(char* head)
 {
-    stringstream ss;
-    ss << from;
-    T foo;
-    ss >> foo;
-    return foo;
+  for (char* i = head+1; *i; i++)
+    if (*i == '?')
+      *i = '0';
+  if (*head == '?')
+    *head = '1';
 }
 
-bool Constract(char* lhs, char* rhs)
+bool EqualRepair(char* lhs, char* rhs)
 {
+  string minv = "#";
+  for (int i = 0; rhs[i]; i++) {
+    if (!(lhs[i] < rhs[i] || rhs[i] == '?'))
+      continue;
+    string t = rhs;
+    if (t[i] == '?')
+      t[i] = min(lhs[i]+1, '9'+0);
+    for (int j = 0; j < i; j++)
+      if (t[j] == '?')
+        t[j] = lhs[j];
+    for (int j = i+1; t[j]; j++)
+      if (t[j] == '?')
+        t[j] = '0';
+    if (string(lhs) < t && (minv == "#" || minv > t))
+      minv = t;
+  }
+  if (minv == "#")
+    return false;
+  strcpy(rhs, minv.data());
   return true;
+}
+
+bool IsOk(char* lhs, char* rhs)
+{
+  int d = strlen(lhs)-strlen(rhs);
+  if (d > 0)
+    return false;
+  if (d < 0) {
+    MinRepair(rhs);
+    return true;
+  }
+  return EqualRepair(lhs, rhs);
 }
 
 int main()
 {
   scanf("%d", &n);
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
     scanf("%s", d[i]);
-  d[0][0] = '0';
-  for (int i = 1; i <= n; i++)
-    if (!Constract(d[i-1], d[i])) {
+  MinRepair(d[0]);
+  for (int i = 1; i < n; i++)
+    if (!IsOk(d[i-1], d[i])) {
       puts("NO");
       return 0;
     }
