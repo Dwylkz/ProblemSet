fc95bacb05be2ae6056b33ed6d3e36272d0a684e
Dwylkz <xellosgeteternal@gmail.com>
2015-01-25 00:36:22 +0800

    fix bc27a

--- bc/27/a/main.cc
+++ bc/27/a/main.cc
@@ -8,12 +8,9 @@
 
 using namespace std;
 
-const int N = 3+5;
+const int N = 3e2+5;
 
-typedef pair<int, int> PII;
-
-PII ps[N];
-int n;
+int n, h[N], r[N];
 
 int main()
 {
@@ -21,25 +18,22 @@ int main()
   scanf("%d", &T);
   while (T--) {
     scanf("%d", &n);
+    memset(h, -1, sizeof(h));
     for (int i = 0; i < n; i++) {
-      ps[i].first = 0;
-      ps[i].second = i;
-    }
-    for (int i = 0; i < n; i++) {
+      int md = 0;
       for (int j = 0; j < 3; j++) {
         int d;
         scanf("%d", &d);
-        ps[i].first = max(ps[i].first, d);
+        md = max(md, d);
       }
+      h[md] = i;
     }
-    sort(ps, ps+n, greater<PII>());
-    for (int i = 0; i < n; i++) {
-      printf("%d", ps[i].second+1);
-      if (i < n-1)
-        putchar(' ');
-      else
-        puts("");
-    }
+    int rank = 0;
+    for (int i = N-1; i >= 0; i--)
+      if (h[i] != -1)
+        r[h[i]] = rank++;
+    for (int i = 0; i < n; i++)
+      printf("%d%c", r[i]+1, i < n-1? ' ': '\n');
   }
   return 0;
 }
