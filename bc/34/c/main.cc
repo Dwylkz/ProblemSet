6d6a195dd29a639ffa423e5a47ca87748b800dc1
Dwylkz <xellosgeteternal@gmail.com>
2015-03-22 10:04:29 +0800

    fix

--- bc/34/c/main.cc
+++ bc/34/c/main.cc
@@ -11,15 +11,64 @@ using namespace std;
 
 typedef long long LL;
 
-const int N = 2e5+5;
+const int N = 5e4+5;
+const LL oo = (-1ull<<63)-1;
 
-int n, w, h, p[N];
+struct Fenwick {
+  LL t[N];
+  void Init()
+  {
+    memset(t, 0, sizeof(t));
+  }
+  Fenwick& Add(int x, int y)
+  {
+    for (x++; x < N; x += -x&x) t[x] += y;
+    return *this;
+  }
+  LL Ask(int x)
+  {
+    LL r = 0;
+    for (x++; x; x -= -x&x) r += t[x];
+    return r;
+  }
+  LL Ask(int x, int y)
+  {
+    return Ask(y)-Ask(x);
+  }
+};
 
-void Ass(LL a, LL s, LL& p, LL& n)
-{
-  if (a >= 0) p += a*s;
-  else n += -1*a*s;
-}
+struct Ans {
+  Ans(LL c, LL h): c(c), h(h) {}
+  friend bool operator < (const Ans& lhs, const Ans& rhs)
+  {
+    return lhs.c < rhs.c || (lhs.c == rhs.c && lhs.h > rhs.h);
+  }
+  friend ostream& operator << (ostream& lhs, const Ans& rhs)
+  {
+    if (rhs.c == oo) return lhs << -1;
+    return lhs << rhs.h << " " << rhs.c;
+  }
+  LL c, h;
+};
+
+Fenwick zkl, cnt;
+int n, w, h, p[3*N];
+
+struct Update {
+  Update(int lower, LL sum, LL all, Ans& ans)
+      : lower(lower), sum(sum), all(all), ans(ans) {}
+  Update& operator () (int h)
+  {
+    if (h < lower) return *this;
+    LL pos = zkl.Ask(h, N-2)-cnt.Ask(h, N-2)*h, neg = cnt.Ask(h)*h-zkl.Ask(h), cost = max(pos, neg);
+    if (neg-pos > all-sum) return *this;
+    ans = min(Ans(cost, h), ans);
+    return *this;
+  }
+  int lower;
+  LL sum, all;
+  Ans& ans;
+};
 
 int main()
 {
@@ -30,30 +79,22 @@ int main()
       scanf("%d", p+i);
       all += p[i];
     }
-    LL minv = B(), maxh = h;
-    if (minv == -1) {
-      puts("-1");
-      continue;
-    }
-
-    LL pos = 0, neg = 0, sum = 0, hs = 0;
-    for (int i = 0; i < w-1; i++) {
+    Ans ans(oo, oo);
+    zkl.Init();
+    cnt.Init();
+    LL sum = 0;
+    for (int i = 0; i < w; i++) {
       sum += p[i];
-      Ass(p[i]-h, 1, pos, neg);
+      zkl.Add(p[i], p[i]);
+      cnt.Add(p[i], 1);
     }
-    for (int i = w-1; i < n+2*w; i++) {
-      Ass(p[i]-h, 1, pos, neg);
-      sum += p[i];
-      if (i-w >= 0) {
-        Ass(p[i-w]-h, -1, pos, neg);
-        sum -= p[i-w];
-      }
-      LL h2 = 2ll*w*h, pot = h2-sum;
-      if (h2 >= sum) continue;
-      h = sum/2;
-      LL cost = min(pos);
+    for (int i = w; i < n+2*w; i++) {
+      sum += p[i]-p[i-w];
+      zkl.Add(p[i], p[i]).Add(p[i-w], -p[i-w]);
+      cnt.Add(p[i], 1).Add(p[i-w], -1);
+      Update(h, sum, all, ans)(h)(sum/w)((sum+w-1)/w);
     }
-    cout << minv << endl;
+    cout << ans << endl;
   }
   return 0;
 }
