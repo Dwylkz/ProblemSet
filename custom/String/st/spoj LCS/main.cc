ee8b38451b529b9e0fc792564f73aeea399c2c11
Dwylkz <xellosgeteternal@gmail.com>
2015-04-12 01:56:26 +0800

    add gcj 2015

--- custom/String/st/spoj LCS/main.cc
+++ custom/String/st/spoj LCS/main.cc
@@ -17,48 +17,69 @@ const int N = 5e5+5;
 
 struct STree {
   struct Node {
-    map<int, Node*> at;
+    struct Pair {
+      Pair(Node* s = NULL, int k = 0, int p = 0): s(s), k(k), p(p) {}
+      Node* s;
+      int k, p;
+    };
+    map<int, Pair> e;
     Node* f;
-    char *k, *p;
   };
-
-  struct Ref {
-    Node* s;
-    char *k, *p;
-
-    CP(Node* s, char* k, char* p): s(s), k(k), p(p) {}
-
-    bool Has(int c)
-    {
-      if (k == p) return (s->at.count(c) > 0);
-      return (*p == c);
-    }
-  };
-
-  static Node pool[N<<1];
-  Node *root;
-  Ref act;
-
+  Node pool[N<<1], *rt, *top;
+  vector<int> data;
+  Node::Pair last;
+  Node* New(Node* f)
+  {
+    top->e.clear();
+    top->f = f;
+    return top++;
+  }
   void Init()
   {
     top = pool;
-    root = pool++;
-    root->f = root->k = root->p = NULL;
-    act = CP(root, NULL, NULL);
+    rt = New(rt);
+    last = Node::Pair(rt, 0, -1);
   }
-
+  int End(int x)
+  {
+    return x < 0? data.size(): x;
+  }
+  bool Has(int c)
+  {
+    if (last.p < End(last.s->p)) return data[last.p] == c;
+    return last.s->e.count(c) > 0;
+  }
+  Node* Add(int c, Node* f)
+  {
+    return NULL;
+  }
+  void Next(int c)
+  {
+  }
+  void NextLink()
+  {
+  }
   void Push(int c)
   {
-    while (!act.Has(c)) {
+    data.push_back(c);
+    Node* pre = NULL;
+    while (!Has(c)) {
+      pre = Add(c, pre);
+      NextLink();
     }
+    Next(c);
   }
 };
 
+STree zkl;
 int n;
+char foo[N];
 
 int main()
 {
-  scanf("%d", &n);
-  printf("%d", n);
+  scanf("%s", foo);
+  zkl.Init();
+  for (int i = 0; foo[i]; i++)
+    zkl.Push(foo[i]);
   return 0;
 }
---
+++ gcj/2015/q/cc/main.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+  int T;
+  scanf("%d", &T);
+  for (int kas = 1; kas <= T; kas++) {
+    printf("Case #%d: ", kas);
+  }
+  return 0;
+}
